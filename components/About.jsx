b52f6c62253beb120779187396f5e319ba7aef26
Jack_work <jacquespariseau93@gmail.com>
2023-06-01 16:27:01 -0400

    editing about section styles, troubleshooting flex layout

--- components/About.jsx
+++ components/About.jsx
@@ -3,19 +3,17 @@ import StackCarousel from "./StackCarousel";
 const About = () => {
     return ( 
 
-        <section id="about" className="py-10 h-screen w-screen">
-            <div className="about-wrapper md:flex max-w-fit ">
-                    <div className="header h-full">
-                        <h2 className="font-bold text-6xl text-blue-light md:-rotate-90 md:m-0 mb-5">About</h2>
-                    </div>
-                    <div className="content text-blue-dark flex flex-col">
-                        <div className="body block text-2xl px-5">
-                            <p className="break-words w-full">I'm Jacques Pariseau, a self-taught <b>web developer</b>.</p>
-                            <p className="break-words w-full">I'm an academically-trained <b>art historian</b> and <b>marketing and graphic design professional</b>.</p>
-                            <p className="break-words w-full">I love learning new things—languages, musical instruments, cooking—but recently, I've focused on learning web development.</p>
-                        </div>
-                        <StackCarousel />
-                    </div>
+        <section id="about" className="py-10 h-screen w-full flex">
+            <div className="header flex-initial flex flex-col items-start justify-start h-full">
+                <h2 className="font-bold text-blue-light md:-rotate-90 md:m-0 mb-5 text-end">About</h2>
+            </div>
+            <div className="content text-blue-dark flex flex-col flex-initial">
+                <div className="body block text-2xl px-5 w-fit">
+                    <p >I'm Jacques Pariseau, a self-taught <b>web developer</b>.</p>
+                    <p >I'm an academically-trained <b>art historian</b> and <b>marketing and graphic design professional</b>.</p>
+                    <p >I love learning new things—languages, musical instruments, cooking—but recently, I've focused on learning web development.</p>
+                </div>
+                <StackCarousel />
             </div>
         </section>
      );
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
     return (
-        <section className="hero flex flex-col items-center w-full md:my-10 my-5 h-full ">
+        <section className="hero flex flex-col items-center w-full md:my-10 my-5 h-screen ">
             <h1 className="font-bold text-hero-lg flex flex-col"><span className="name">Jacques</span><span className="name">Pariseau</span></h1>
             <h2 className="font-thin text-hero-sm">web developer</h2>
         </section> 
