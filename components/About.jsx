d74a55b8199a84d39664db822baa8b13785b69b6
Jack_work <jacquespariseau93@gmail.com>
2023-06-02 16:03:10 -0400

    more or less finished styling about section

--- components/About.jsx
+++ components/About.jsx
@@ -3,15 +3,13 @@ import StackCarousel from "./StackCarousel";
 const About = () => {
     return ( 
 
-        <section id="about" className="py-10 h-screen w-full flex">
-            <div className="header flex-initial flex flex-col items-start justify-start h-full">
-                <h2 className="font-bold text-blue-light md:-rotate-90 md:m-0 mb-5 text-end">About</h2>
-            </div>
+        <section id="about" className="py-36 h-fit w-full container md:columns-2">
+            <h2 className="font-bold text-blue-light text-8xl mb-2 break-after-column md:text-end md:pr-5">About</h2>
             <div className="content text-blue-dark flex flex-col flex-initial">
-                <div className="body block text-2xl px-5 w-fit">
-                    <p >I'm Jacques Pariseau, a self-taught <b>web developer</b>.</p>
-                    <p >I'm an academically-trained <b>art historian</b> and <b>marketing and graphic design professional</b>.</p>
-                    <p >I love learning new things—languages, musical instruments, cooking—but recently, I've focused on learning web development.</p>
+                <div className="body block text-2xl w-fit">
+                    <p className="mb-5">I'm Jacques Pariseau, a self-taught <b>web developer</b>.</p>
+                    <p className="mb-5">I'm an academically-trained <b>art historian</b> and <b>marketing and graphic design professional</b>.</p>
+                    <p className="mb-5">I love learning new things—languages, musical instruments, cooking—but recently, I've focused on learning web development.</p>
                 </div>
                 <StackCarousel />
             </div>
@@ -20,3 +18,5 @@ const About = () => {
 }
  
 export default About;
+
+// md:-translate-x-1/3 md:origin-top-right md:left-0 md:top-0 md:-rotate-90 mb-4 md:mb-0
