9f4dc89525b3c67a637284364e0250a0298e4e17
Jack_work <jacquespariseau93@gmail.com>
2023-06-07 12:28:38 -0400

    fixing file path for public images

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
-import tipCalculator from '../public/images/tipCalculator.png'
-import ageCalculator from '../public/images/ageCalculator.png'
-import cssIllustration from '../public/images/cssIllustrationLandingPage.png'
-import randomColor from '../public/images/randomColor.png'
+import tipCalculator from '/images/tipCalculator.png'
+import ageCalculator from '/images/ageCalculator.png'
+import cssIllustration from '/images/cssIllustrationLandingPage.png'
+import randomColor from '/images/randomColor.png'
 
 module.exports.education = [
     {
