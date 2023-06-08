145365bef7cf15579af62efc8ed06201c899e21c
Jack_work <jacquespariseau93@gmail.com>
2023-06-08 11:55:33 -0400

    updating next config for working with next images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-const withImages =require('next-images')
-module.exports= withImages();
+const withImages = require('next-images')
+module.exports = withImages({
+  webpack(config, options) {
+    return config
+  }
+})
 module.exports = nextConfig
