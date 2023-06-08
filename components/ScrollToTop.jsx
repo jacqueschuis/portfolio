c58bf70078e647aae9e82ad6e298c34da59067f6
Jack_work <jacquespariseau93@gmail.com>
2023-06-08 16:09:02 -0400

    styling the scroll to top button, will be adding a transition to check if the page has been scrolled before it appears

--- components/ScrollToTop.jsx
+++ components/ScrollToTop.jsx
@@ -3,9 +3,9 @@ let scroll = Scroll.animateScroll;
 
 const ScrollToTop = () => {
     return ( 
-        <div className="fixed w-full h-[80px] bottom-0 mb-5 text-orange-dark flex items-center justify-center md:justify-end" >
+        <div className="fixed flex items-center justify-center w-[40px] h-[40px] md:w-[50px] md:h-[50px] bottom-0 m-5 text-orange-dark transition-all hover:text-orange-light cursor-pointer right-0 bg-white rounded-full shadow-lg" onClick={() => {scroll.scrollToTop()}} >
             <svg xmlns="http://www.w3.org/2000/svg"  width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" >
-                <path fill-rule="evenodd" className="transition-all hover:text-orange-light cursor-pointer" onClick={() => {scroll.scrollToTop()}} d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z"/>
+                <path fill-rule="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z"/>
             </svg>
         </div>
      );
