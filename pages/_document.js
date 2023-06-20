fd1591ab8607918a26c16476474d4313a4fb1cc5
Jack_work <jacquespariseau93@gmail.com>
2023-06-20 10:18:19 -0400

    adding apple touch icon

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ const MyDocument = () => {
             <Head>
                 <link rel="stylesheet" href="./globals.css" />
                 <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" sizes="any"/>
+                <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" sizes="180x180" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,500;0,800;1,300;1,500;1,800&display=swap" rel="stylesheet" />
