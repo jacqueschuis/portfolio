5b85de91c9fbbed149fb42024358f259d2233754
Jack_work <jacquespariseau93@gmail.com>
2023-06-26 16:30:15 -0400

    playing around with react spring for a hero text animation, unsure if it will be able to be accomplished

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,23 +1,51 @@
-import { useTrail, useTransition, animated } from "@react-spring/web";
+import { useTrail, useTransition, animated, useSpring } from "@react-spring/web";
 import {v4 as uuidv4} from 'uuid'
 
 const Hero = () => {
-    const firstName = 'Jacques';
-    const lastName = 'Pariseau';
+    const firstName = [...'Jacques'];
+    const lastName = [...'Pariseau'];
+
+    const [appear, appearApi] = useSpring(() => ({
+        from: {opacity: 0},
+        opacity: 1
+    }))
+
+    // const base = {
+    //     config: {mass: 5, tension: 2000, friction: 200},
+    //     from: { translate: '-100%', scale: 0 },
+    //     to: { translate: '0%', scale: 1 },
+    // }
+
+    // const firstNameSprings = useSprings(firstName.length, firstName.map((t,i) => ({...base, delay: 200*i})))
+
+    const firstNameTrails = useTrail(
+        firstName.length,
+        {
+            from: { opacity: 0, y: 2000, rotate: '100deg' },
+            opacity: 1,
+            rotate: '0deg',
+            x: 0,
+        }
+    )
     
 
     return (
         <section className="hero flex flex-col items-center w-full mb-32 h-fit py-36">
-            <h1 className="font-bold text-hero-lg flex flex-col">
+            <h1 style={...appear} className="font-bold text-hero-lg flex flex-col">
                 <span className="name">
-                    {firstName.split("").map(el => {
+                    {/* {firstNameTrails.map(({...animation}, index) => {
+                        return (
+                            <animated.span style={animation} key={`${uuidv4()}`}>{firstName[index]}</animated.span>
+                        )
+                    })} */}
+                    {firstName.map((el) => {
                         return (
                             <span key={`${uuidv4()}`} className="hero-letter animate">{el}</span>
                         )
                     })}
                 </span>
                 <span className="name">
-                    {lastName.split("").map(el => {
+                    {lastName.map(el => {
                         return (
                             <span key={`${uuidv4()}`} className="hero-letter animate">{el}</span>
                         )
