dd55c4bed41253fc9341a856479fdf12101fb25c
jacqueschuis <jacquespariseau93@gmail.com>
2023-06-25 21:29:10 -0400

    using a map to make individual span wrappers around each of the hero letters for hopefully animating

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,28 @@
-import { useState } from "react";
-import { useTrail, animated } from "@react-spring/web";
+import { useTrail, useTransition, animated } from "@react-spring/web";
 
 const Hero = () => {
-    const [state, setState] = useState(true);
-
-    const items = ["jacques", "pariseau"];
-    const config = {mass: 5, tension: 2000, friction: 200};
+    const firstName = 'Jacques';
+    const lastName = 'Pariseau';
+    
 
     return (
         <section className="hero flex flex-col items-center w-full mb-32 h-fit py-36">
-            <h1 className="font-bold text-hero-lg flex flex-col"><span className="name">Jacques</span><span className="name">Pariseau</span></h1>
+            <h1 className="font-bold text-hero-lg flex flex-col">
+                <span className="name">
+                    {firstName.split("").map(el => {
+                        return (
+                            <span className="hero-letter animate">{el}</span>
+                        )
+                    })}
+                </span>
+                <span className="name">
+                    {lastName.split("").map(el => {
+                        return (
+                            <span className="hero-letter animate">{el}</span>
+                        )
+                    })}
+                </span>
+            </h1>
             <h2 className="font-thin text-hero-sm">web developer</h2>
         </section> 
      );
